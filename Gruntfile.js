58268b0e6c4a3b482dd6cdf32aa001f73ea7ec90
xiejun <xiejun@umiit.cn>
2016-03-31 04:06:05 +0800

    init

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib: {
+        src: ['lib/**/*.js']
+      },
+      tasks: {
+        src: ['tasks/**/*.js']
+      },
+    },
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint']);
+
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# grunt-umi-qiniu
+
+grunt-fis插件，用于将静态文件上传到qiniu服务器
